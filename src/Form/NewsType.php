c39cdc441582c91ddc2e7df66baa3076ef93961f
Jaanioo <jan.palen97@gmail.com>
2023-10-24 18:33:29 +0200

    Make whole week6 exercises

---
+++ src/Form/NewsType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Tytuł',
+                'attr' => [
+                    'placeholder' => 'Tytuł wiadomości',
+                ],
+                'help' => 'Wpisz tytuł wiadomości',
+            ])
+            ->add('text', TextareaType::class)
+            ->add('createdAt', DateType::class, [
+                'widget' => 'single_text',
+                'data_class' => \DateTimeImmutable::class
+            ])
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Save',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
